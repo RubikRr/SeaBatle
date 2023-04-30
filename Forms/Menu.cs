e212c8e6cb47a44cac5455645e38eaac477dc8e9
Ukropchik <vova.chakalov.2003@gmail.com>
2023-04-30 20:03:39 +0300

    переименовал форму и выставил ее в центр

---
+++ Forms/Menu.cs
@@ -0,0 +1,12 @@
+namespace SeaBatle
+{
+    public partial class Menu : Form
+    {
+
+        public Menu()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+    }
+}

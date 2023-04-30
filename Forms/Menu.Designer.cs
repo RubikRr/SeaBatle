69da6333ebbbd6cf91760dbf687cbfe4731ec55e
Ukropchik <vova.chakalov.2003@gmail.com>
2023-04-30 21:06:36 +0300

    Добавил конфиг файл и 3 кнопки в меню

--- Forms/Menu.Designer.cs
+++ Forms/Menu.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(174, 139);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 39);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(174, 249);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 40);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(174, 344);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 46);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(174, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Морской бой";
             // 
             // Menu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(484, 461);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Menu";
             Text = "Menu";
+            Load += Menu_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
     }
 }
--- Forms/Menu.cs
+++ Forms/Menu.cs
@@ -1,12 +1,24 @@
+using System.Configuration;
+
 namespace SeaBatle
 {
     public partial class Menu : Form
     {
+        int minWidth = int.Parse(ConfigurationManager.AppSettings.Get("minMenuWidth"));
+        int minHeight = int.Parse(ConfigurationManager.AppSettings.Get("minMenuHeight"));
 
         public Menu()
         {
             InitializeComponent();
             CenterToScreen();
         }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            this.MinimumSize = new Size(minWidth, minHeight);
+
+        }
+
+
     }
 }
